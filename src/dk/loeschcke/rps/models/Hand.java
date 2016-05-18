159469a7ec6599b645e217b347089b2a4ee10147
Stefan Loeschcke <stefan@loeschcke.dk>
2016-05-18 18:51:38 +0100

    Modelling a simple Hand with a strategy

---
+++ src/dk/loeschcke/rps/models/Hand.java
@@ -0,0 +1,25 @@
+package dk.loeschcke.rps.models;
+
+import dk.loeschcke.rps.strategy.HandStrategy;
+
+/**
+ * Created by sbugge on 18/05/16.
+ */
+public class Hand {
+
+    private final String name;
+    private HandStrategy strategy;
+
+    public Hand(String name, HandStrategy handStrategy) {
+        this.name = name;
+        this.strategy = handStrategy;
+    }
+
+    public Shape throwShape() {
+        return strategy.execute();
+    }
+
+    public String getName() {
+        return name;
+    }
+}
