1e608c69a009e85c9517acfef3920cf4b9e2fccf
Stefan Loeschcke <stefan@loeschcke.dk>
2016-05-19 10:11:07 +0100

    Refactoring Game class and moving game loop to Main class

--- src/dk/loeschcke/rps/Game.java
+++ src/dk/loeschcke/rps/Game.java
@@ -1,34 +1,44 @@
 package dk.loeschcke.rps;
 
-import dk.loeschcke.rps.logic.ScoreCalculator;
 import dk.loeschcke.rps.models.Hand;
 import dk.loeschcke.rps.models.Shape;
-import dk.loeschcke.rps.strategy.FixedHandStrategy;
-import dk.loeschcke.rps.strategy.RandomHandStrategy;
-import dk.loeschcke.rps.util.RandomGenerator;
 
 /**
  * Created by sbugge on 18/05/16.
  */
 public class Game {
 
-    public Game(int iterations) {
+    private Hand h1;
+    private Hand h2;
 
-        Hand h1 = new Hand("1", new FixedHandStrategy(Shape.ROCK));
-        Hand h2 = new Hand("2", new RandomHandStrategy(new RandomGenerator()));
-
-        ScoreCalculator sc = new ScoreCalculator();
-
-        int i = 0;
-        while (i < iterations) {
-
-            Shape s1 = h1.throwShape();
-            Shape s2 = h2.throwShape();
-
-            System.out.println((i+1) + ": " + h1 + " = " + s1 + "; " + h2 + " = " + s2 + "; winner = " + sc.calculate(s1, s2));
-
-            i++;
-        }
-
+    public Game(Hand h1, Hand h2) {
+        this.h1 = h1;
+        this.h2 = h2;
     }
+
+    /**
+     * @param s1 Shape 1
+     * @param s2 Shape 2
+     * @return circular method that will always return either 0, 1 or 2.
+     *          0 = Tie
+     *          1 = s1 beats s2
+     *          2 = s2 beats s1
+     */
+    public int calculate(Shape s1, Shape s2) {
+        return (s1.getValue() + 3 - s2.getValue()) % 3;
+    }
+
+    public void play() {
+
+        Shape s1 = h1.throwShape();
+        Shape s2 = h2.throwShape();
+
+        int result = calculate(s1, s2);
+
+        System.out.println(h1 + " = " + s1 + "; " + h2 + " = " + s2 + "; winner = " + result);
+        if (result == 0) {
+            play();
+        }
+    }
+
 }
--- src/dk/loeschcke/rps/Main.java
+++ src/dk/loeschcke/rps/Main.java
@@ -1,5 +1,11 @@
 package dk.loeschcke.rps;
 
+import dk.loeschcke.rps.models.Hand;
+import dk.loeschcke.rps.models.Shape;
+import dk.loeschcke.rps.strategy.FixedHandStrategy;
+import dk.loeschcke.rps.strategy.RandomHandStrategy;
+import dk.loeschcke.rps.util.RandomGenerator;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,6 +13,19 @@ public class Main {
         if (args != null && args.length > 0) {
             iterations = Integer.parseInt(args[0], 10);
         }
-        new Game(iterations);
+
+        Hand h1 = new Hand("1", new FixedHandStrategy(Shape.ROCK));
+        Hand h2 = new Hand("2", new RandomHandStrategy(new RandomGenerator()));
+
+        Game game = new Game(h1, h2);
+
+        int i = 0;
+        while (i < iterations) {
+
+            game.play();
+
+            i++;
+        }
+
     }
 }
